e0c24ced53f9f0605bec82a197173a4feda4ff96
Jonathan Robin Langford <jrobinlangford@gmail.com>
2018-07-25 02:00:20 -0500

    Update installation script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://github.com/jrlangford/docker-server-manager.git
 cp $P_DIR/server.py .
 chmod +x server.py
 
-cp $P_DIR/serverconf.example.json serverconf.json
+cp $P_DIR/serverconf.example.json .
 cp $P_DIR/nginx.conf.jn2 .
 
 rm -rf $P_DIR
